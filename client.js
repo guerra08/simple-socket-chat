b90871997fd06801117566cca87e2d1d1911a125
Bruno Guerra <gguerrabruno@outlook.com>
2020-04-02 20:25:44 -0300

    Chat loop.

--- client.js
+++ client.js
@@ -1,27 +1,40 @@
 const PORT = 33333;
 const HOST = '127.0.0.1';
-
 const prompt = require('prompts')
-
 const dgram = require('dgram');
+
 const greeting = "I'm alive!" ;
 
 const client = dgram.createSocket('udp4');
 
-client.send(greeting, 0, greeting.length, PORT, HOST, function (err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST + ':' + PORT);
-});
+sendMessage = async (msg) => {
 
-(async () => {
+    if(!msg) return
+
+    client.send(msg, 0, msg.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+    })
+} 
+
+chatLoop = async () => {
     const response = await prompt({
         type: 'text',
         name: 'message',
         message: 'Digita aí fera: '
-    });
+    })
 
-    client.send(response.message, 0, response.message.length, PORT, HOST, function (err, bytes) {
-        if (err) throw err;
-        console.log('UDP message sent to ' + HOST + ':' + PORT);
-    });
+    if(!response.message) return
+
+    await sendMessage(response.message)
+
+    chatLoop()
+}
+
+(async() => {
+    try{
+        await sendMessage(greeting)
+        await chatLoop()
+    }catch(e){
+        //Logs the error
+    }
 })()
