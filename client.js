713b17794e95a1647d0a1d343f7899f3e839f87d
Bruno Guerra <gguerrabruno@outlook.com>
2020-04-05 19:16:24 -0300

    TCP simple example option

--- client.js
+++ client.js
@@ -2,39 +2,52 @@ const PORT = 33333;
 const HOST = '127.0.0.1';
 const prompt = require('prompts')
 const dgram = require('dgram');
-
+const argv = require('yargs').argv
 const greeting = "I'm alive!" ;
 
-const client = dgram.createSocket('udp4');
-
-sendMessage = async (msg) => {
-
-    if(!msg) return
-
-    client.send(msg, 0, msg.length, PORT, HOST, function (err, bytes) {
-        if (err) throw err;
-    })
-} 
-
-chatLoop = async () => {
-    const response = await prompt({
-        type: 'text',
-        name: 'message',
-        message: 'Digita aí fera: '
-    })
-
-    if(!response.message) return
-
-    await sendMessage(response.message)
-
-    chatLoop()
+if(!argv){
+    console.log("Invalid args.")
+    return
+}
+switch(argv.type){
+    case 'tcp': {}
+    case 'udp': createUdp()
 }
 
-(async() => {
-    try{
-        await sendMessage(greeting)
-        await chatLoop()
-    }catch(e){
-        //Logs the error
+function createUdp(){
+
+    const client = dgram.createSocket('udp4');
+
+    sendMessage = async (msg) => {
+    
+        if(!msg) return
+    
+        client.send(msg, 0, msg.length, PORT, HOST, function (err, bytes) {
+            if (err) throw err;
+        })
+    } 
+    
+    chatLoop = async () => {
+        const response = await prompt({
+            type: 'text',
+            name: 'message',
+            message: 'Digita aí fera: '
+        })
+    
+        if(!response.message) return
+    
+        await sendMessage(response.message)
+    
+        chatLoop()
     }
-})()
+    
+    (async() => {
+        try{
+            await sendMessage(greeting)
+            await chatLoop()
+        }catch(e){
+            
+        }
+    })()
+
+}
--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 const PORT = 33333;
 const HOST = '127.0.0.1';
 
+const argv = require('yargs').argv
 const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
+const net = require('net');
 
-server.on('listening', function () {
-    var address = server.address();
-    console.log('UDP Server listening on ' + address.address + ':' + address.port);
-});
+if (!argv) {
+    return;
+}
+switch (argv.type) {
+    case 'tcp': {
+        createTcp() 
+        break
+    }
+    case 'udp': {
+        createUdp()
+        break
+    }
+}
 
-server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port + ' - ' + message);
-});
+function createTcp() {
+    const server = net.createServer(function (socket) {
+        socket.write('Echo server\r\n');
+        socket.pipe(socket);
+    });
 
-server.bind(PORT, HOST);
+    server.listen(PORT, HOST);
+
+    server.on('listening', function(){
+        const address = server.address();
+        console.log('TCP Server listening on ' + address.address + ':' + address.port);
+    })
+}
+
+function createUdp() {
+    const server = dgram.createSocket('udp4');
+
+    server.on('listening', function () {
+        const address = server.address();
+        console.log('UDP Server listening on ' + address.address + ':' + address.port);
+    });
+
+    server.on('message', function (message, remote) {
+        console.log(remote.address + ':' + remote.port + ' - ' + message);
+    });
+
+    server.bind(PORT, HOST);
+}
